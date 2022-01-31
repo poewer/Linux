93f00208ee80cf8664f5c33b48852ecbb2a91413
Michał Białek <poewer1@gmail.com>
2022-01-31 14:25:52 +0100

    multiplication table

---
+++ homework_Linux/tabliczka_mnozenia_Michal_Bialek_cp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Podaj wymiary tabliczki mnożenia A x B"
+echo "Podaj A"
+read A
+echo "Podaj B"
+read B
+
+for ((i=1; i <= $A; i++));
+do   
+    for ((j=1; j <= $B; j++));
+    do 
+        nowa=$(($i * $j));
+        echo "$i * $j = $nowa"
+    done
+done
